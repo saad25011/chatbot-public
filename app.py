2e1960dc8a643840298d7401a0187de43f801d32
saad25011 <thesaadkhan01@gmail.com>
2024-05-10 09:22:35 -0400

    changings in app

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def add():
     todos.append({"ans": a})
     return redirect(url_for("index"))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
